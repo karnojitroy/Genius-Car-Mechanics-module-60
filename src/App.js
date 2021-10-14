8f250f844f996029bdbddfe2bb72986d4d0a97a5
karnojitroy <karnojitroy@gmail.com>
2021-10-14 22:37:21 +0600

    1st

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import Home from './Pages/Home/Home/Home';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Switch>
+          <Route>
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
